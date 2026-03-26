950e00e051ac1266c4293500ba00e9eed4f0aca6
Breno Façanha <brenofacanha.ti@gmail.com>
2026-03-25 21:56:48 -0300

    test /

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API de Geocodificação');
+});
+
 app.post('/geocode', async (req, res) => {
     const { address } = req.body;
     console.log('Endereço recebido:', address);
